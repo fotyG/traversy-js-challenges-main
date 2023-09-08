933f0f72a3516b5b633d49c32ff805d0f6c854d1
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-08 16:52:16 +0200

    Section 2: [11/15]

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,11 @@
-function findMaxNumber() {}
+function findMaxNumber(numArr) {
+  let maxNum = 0;
+
+  for (let i = 0; i < numArr.length; i++) {
+    if (numArr[i] > maxNum) maxNum = numArr[i];
+  }
+
+  return maxNum;
+}
 
 module.exports = findMaxNumber;
--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,16 @@
-function titleCase() {}
+function titleCase(str) {
+  const newStr = str.toLowerCase().split(" ");
+  const result = [];
+
+  for (let i = 0; i < newStr.length; i++) {
+    if (newStr[i].length > 0) {
+      result.push(newStr[i].slice(0, 1).toUpperCase() + newStr[i].slice(1));
+    }
+  }
+
+  return result.join(" ");
+}
+
+titleCase("hedklaodo  odksapdkwgog jgijJ JJJ dsd");
 
 module.exports = titleCase;
