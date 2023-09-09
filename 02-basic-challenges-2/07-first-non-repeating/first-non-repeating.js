0c30ecad546a1ad97c01b35a0ea90e92b75c4838
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-09 17:31:45 +0200

    Section 3: 8/10

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,20 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  const object = {};
 
+  for (let i = 0; i < str.length; i++) {
+    object[str[i]] = (object[str[i]] || 0) + 1;
+  }
+
+  for (let key in object) {
+    if (object[key] === 1) return key;
+  }
+
+  return null;
+}
+
+const result1 = findFirstNonRepeatingCharacter("aabbcdd");
+const result2 = findFirstNonRepeatingCharacter("aabbccddeeffghh");
+
+console.log(result1);
+console.log(result2);
 module.exports = findFirstNonRepeatingCharacter;
--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,33 @@
-function diceGameSimulation() {}
+function diceGameSimulation(num) {
+  const resultArr = [];
+  let result;
 
+  for (let i = 1; i <= num; i++) {
+    const dice1 = rollDice();
+    const dice2 = rollDice();
+    const sum = dice1 + dice2;
+    if (sum === 7 || sum === 11) result = "win";
+    else if (sum === 2 || sum === 3 || sum === 12) result = "lose";
+    else result = "roll again";
+    let localObj = {
+      dice1,
+      dice2,
+      sum,
+      result,
+    };
+
+    resultArr.push(localObj);
+  }
+
+  return resultArr;
+}
+
+function rollDice() {
+  return Math.ceil(Math.random() * 6);
+}
+
+const result = diceGameSimulation(10);
+
+console.log(rollDice());
+console.log(result);
 module.exports = diceGameSimulation;
