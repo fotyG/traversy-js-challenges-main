368b9c1a3cc78c3bf37c947d34bf16125970bc80
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-09 15:07:56 +0200

    Section 3: 6/10

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,13 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  return Array.from(new Set(str)).join("") === str;
+}
+
+const result1 = areAllCharactersUnique("abcdefg");
+const result2 = areAllCharactersUnique("abcdefgA");
+const result3 = areAllCharactersUnique("programming");
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
 
 module.exports = areAllCharactersUnique;
