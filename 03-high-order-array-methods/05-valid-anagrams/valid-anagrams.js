6ea59aa47c76032a4e264b2ed02d8b702cc82467
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-10 12:56:34 +0200

    Section 4: 5/10

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,21 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  if (str1.length !== str2.length) return false;
+  const arr1 = [...str1];
+  const arr2 = [...str2];
 
+  const freq1 = arr1.reduce((obj, char) => {
+    obj[char] = (obj[char] || 0) + 1;
+    return obj;
+  }, {});
+
+  const freq2 = arr2.reduce((obj, char) => {
+    obj[char] = (obj[char] || 0) + 1;
+    return obj;
+  }, {});
+
+  return Object.keys(freq1).every((char) => freq1[char] === freq2[char]);
+}
+const result = validAnagrams("app", "ppab");
+
+console.log(result);
 module.exports = validAnagrams;
