dc6ba1b588fbfdc138e24e9e849258e139a08ce3
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-10 07:36:47 +0200

    Section 3: 10/10

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,10 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(arr) {
+  return `(${arr.slice(0, 3).join("")}) ${arr.slice(3, 6).join("")}-${arr
+    .slice(6)
+    .join("")}`;
+}
 
+const result = formatPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+
+console.log(result);
 module.exports = formatPhoneNumber;
--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,17 @@
-function validateEmail() {}
+function validateEmail(email) {
+  const part = email.split("@");
+  const part2 = part[1]?.split(".");
 
+  if (!part2 || part.length !== 2 || part[0] === "") return false;
+  if (part2.length === 2) return true;
+  return false;
+}
+
+const result1 = validateEmail("brad@gmail.com");
+const result2 = validateEmail("bradgmailcom");
+const result3 = validateEmail("user@domain");
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
 module.exports = validateEmail;
