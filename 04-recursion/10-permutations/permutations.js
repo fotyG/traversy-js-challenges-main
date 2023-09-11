75efbe5eda2ec5f70040f321a667e037f07348a4
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-11 20:43:14 +0200

    Section 5: 10/10

--- 04-recursion/10-permutations/permutations.js
+++ 04-recursion/10-permutations/permutations.js
@@ -1,4 +1,23 @@
-function permutations() {}
+function permutations(str) {
+  const result = [];
+
+  if (str.length === 0) {
+    result.push("");
+    return result;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const firstChar = str[i];
+    const restChars = str.slice(0, i) + str.slice(i + 1);
+    const subPermutations = permutations(restChars);
+
+    for (let j = 0; j < subPermutations.length; j++) {
+      result.push(firstChar + subPermutations[j]);
+    }
+  }
+
+  return result;
+}
 
 const result = permutations("abc");
 
