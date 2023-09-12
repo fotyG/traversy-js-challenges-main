fdde58723314dc830ae1a6ddadef0947f4029d7c
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-12 09:49:08 +0200

    Section 6: 10/10

--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,3 +1,21 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+  let maxSum = -Infinity;
+  for (let i = 0; i <= arr.length - k; i++) {
+    let currentSum = 0;
 
+    for (let j = i; j < i + k; j++) {
+      currentSum += arr[j];
+    }
+
+    maxSum = Math.max(maxSum, currentSum);
+  }
+
+  return maxSum;
+}
+
+const arr = [2, 5, 3, 1, 11, 7, 6, 4];
+
+const result = maxSubarraySum(arr, 3);
+
+console.log(result);
 module.exports = maxSubarraySum;
--- 05-complexity/10-max-subarray-linear/max-subarray-linear.js
+++ 05-complexity/10-max-subarray-linear/max-subarray-linear.js
@@ -1,3 +1,36 @@
-function maxSubarraySum() {}
+// function maxSubarraySum(arr, k) {
+//   let maxSum = -Infinity;
 
+//   for (let i = 0; i <= arr.length - k; i++) {
+//     const currentSum = arr.slice(i, i + k).reduce((sum, curr) => sum + curr, 0);
+//     maxSum = Math.max(maxSum, currentSum);
+//   }
+
+//   return maxSum;
+// }
+
+function maxSubarraySum(arr, k) {
+  let currentSum = 0;
+  let maxSum = 0;
+
+  for (let i = 0; i < k; i++) {
+    maxSum += arr[i];
+  }
+
+  currentSum = maxSum;
+
+  for (let i = k; i < arr.length; i++) {
+    currentSum = currentSum - arr[i - k] + arr[i];
+
+    maxSum = Math.max(currentSum, maxSum);
+  }
+
+  return maxSum;
+}
+
+const arr = [-2, -5, -3, -1, -11, -7, -6, -4];
+
+const result = maxSubarraySum(arr, 4);
+
+console.log(result);
 module.exports = maxSubarraySum;
