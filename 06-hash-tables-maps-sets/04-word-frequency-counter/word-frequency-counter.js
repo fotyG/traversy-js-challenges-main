285fe2df4033e3d1b7bbe2837ea7520a22824cb2
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-12 10:59:27 +0200

    Section 7: 3/14

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,22 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(str) {
+  const wordArr = str.toLowerCase().split(/\W+/);
+  const wordMap = new Map();
 
+  wordArr.forEach((word) => {
+    if (wordMap.has(word)) {
+      const value = wordMap.get(word);
+      wordMap.set(word, value + 1);
+    } else {
+      if (word !== "") wordMap.set(word, 1);
+    }
+  });
+
+  return wordMap;
+}
+
+const result = wordFrequencyCounter(
+  "The quick brown fox jumps over the lazy dog dog dog."
+);
+
+console.log(result);
 module.exports = wordFrequencyCounter;
