dd1b8b4feef26223dd2606bf467088ae028c764b
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-13 16:55:43 +0200

    Section 8: 4/17

--- 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
+++ 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
@@ -1,5 +1,21 @@
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-function reverseStringStack() {}
+function reverseStringStack(str) {
+  const stack = new Stack();
+  let reversed = "";
 
+  for (const letter of str) {
+    stack.push(letter);
+  }
+
+  while (!stack.isEmpty()) {
+    reversed += stack.pop();
+  }
+
+  return reversed;
+}
+
+const result = reverseStringStack("Hello World!");
+
+console.log(result);
 module.exports = reverseStringStack;
--- 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
+++ 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
@@ -1,5 +1,22 @@
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-function balancedParenthesis() {}
+function balancedParenthesis(str) {
+  const stack = new Stack();
 
+  for (let item of str) {
+    if (item === "(") {
+      stack.push(item);
+    } else if (item === ")") {
+      if (stack.isEmpty()) return false;
+      stack.pop();
+    }
+  }
+
+  return stack.isEmpty();
+}
+
+const result1 = balancedParenthesis("()()()");
+const result2 = balancedParenthesis("()(");
+
+console.log(result1, result2);
 module.exports = balancedParenthesis;
