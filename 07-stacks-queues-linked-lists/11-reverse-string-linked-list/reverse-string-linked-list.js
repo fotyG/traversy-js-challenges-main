39abc08a292072aa31daf84bc5eae94d66b1eff5
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-14 10:05:14 +0200

    Section 8: 11/17

--- 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
+++ 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
@@ -1,5 +1,24 @@
-const { LinkedList } = require('./linked-list');
+const { LinkedList } = require("./linked-list");
 
-function reverseStringLinkedList() {}
+function reverseStringLinkedList(str) {
+  const list = new LinkedList();
 
+  for (let i = str.length - 1; i >= 0; i--) {
+    list.add(str[i]);
+  }
+
+  let reversed = "";
+  let current = list.head;
+
+  while (current !== null) {
+    reversed += current.data;
+    current = current.next;
+  }
+
+  return reversed;
+}
+
+const result = reverseStringLinkedList("Hello World!");
+
+console.log(result);
 module.exports = reverseStringLinkedList;
