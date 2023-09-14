960a8124e904a61edbebd38583137c71b9be9958
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-14 09:08:20 +0200

    Section 8: 10/17

--- 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
+++ 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
@@ -1,5 +1,21 @@
-const Queue = require('./queue');
+const Queue = require("./queue");
 
-const reverseStringWithQueue = (str) => {};
+const reverseStringWithQueue = (str) => {
+  const queue = new Queue();
+  let reversed = "";
 
+  for (let i = str.length - 1; i >= 0; i--) {
+    queue.enqueue(str[i]);
+  }
+
+  for (const item in str) {
+    reversed += queue.dequeue();
+  }
+
+  return reversed;
+};
+
+const result = reverseStringWithQueue("Hello World!");
+
+console.log(result);
 module.exports = reverseStringWithQueue;
--- 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
+++ 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
@@ -1,6 +1,26 @@
-const Queue = require('./queue');
-const Stack = require('./stack');
+const Queue = require("./queue");
+const Stack = require("./stack");
 
-function isPalindromeQueueStack() {}
+function isPalindromeQueueStack(str) {
+  const stack = new Stack();
+  const queue = new Queue();
 
+  const cleanStr = str.toLowerCase().split(/\W+/).join("");
+
+  for (let char of cleanStr) {
+    stack.push(char);
+    queue.enqueue(char);
+  }
+
+  for (let length in cleanStr) {
+    if (stack.pop() !== queue.dequeue()) return false;
+  }
+
+  return true;
+}
+
+const result1 = isPalindromeQueueStack("A man, a plan, a canal: Panama");
+const result2 = isPalindromeQueueStack("Hello");
+
+console.log(result1, result2);
 module.exports = isPalindromeQueueStack;
