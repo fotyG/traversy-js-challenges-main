5665b8f832d7bf73c1d83792089fbe9cccf25c01
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-14 11:26:51 +0200

    Section 8: 15/17

--- 07-stacks-queues-linked-lists/15-doubly-linked-list-implementation/doubly-linked-list-run.js
+++ 07-stacks-queues-linked-lists/15-doubly-linked-list-implementation/doubly-linked-list-run.js
@@ -1,2 +1,12 @@
-const DoublyLinkedList = require('./doubly-linked-list');
+const DoublyLinkedList = require("./doubly-linked-list");
 
+const list = new DoublyLinkedList();
+
+list.append(200);
+list.append(300);
+list.append(400);
+
+list.prepend(100);
+
+list.insertAt(2, "New");
+list.printAll();
