f72daeb00ae5446f62f9585a5b35856da021e8d0
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-15 14:27:48 +0200

    Section 9: 6/16

--- 08-binary-trees-graphs/06-maximum-depth/maximum-depth-run.js
+++ 08-binary-trees-graphs/06-maximum-depth/maximum-depth-run.js
@@ -1,7 +1,15 @@
-const { Node, maxDepth } = require('./maximum-depth');
+const { Node, maxDepth } = require("./maximum-depth");
 
 // Create the binary tree:    3
 //                           / \
 //                          9  20
 //                             / \
 //                            15  7
+
+const root = new Node(3);
+root.left = new Node(9);
+root.right = new Node(20);
+root.right.left = new Node(15);
+root.right.right = new Node(7);
+
+console.log(maxDepth(root));
