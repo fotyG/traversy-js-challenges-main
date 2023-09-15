0f384692992aa0a8e027fdf37d200525cc1daa0e
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-15 17:02:21 +0200

    Section 9: 8/16

--- 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
+++ 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
@@ -7,12 +7,11 @@ class Node {
 }
 
 function maxDepth(root) {
-  if (!root) return [];
+  if (!root) return 0;
 
   const leftDepth = maxDepth(root.left);
   const rightDepth = maxDepth(root.right);
 
-  console.log(root.left, root.right);
   return Math.max(leftDepth, rightDepth) + 1;
 }
 
