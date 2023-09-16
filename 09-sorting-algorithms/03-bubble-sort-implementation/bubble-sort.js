81f4776f1b196aaf0d989a8c062c5c7fa5a8f2e1
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-16 12:46:01 +0200

    finish

--- 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
+++ 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
@@ -1,3 +1,40 @@
-function bubbleSort() {}
+// function bubbleSort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length - i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         const temp = arr[j];
 
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+
+//   return arr;
+// }
+
+function bubbleSort(arr) {
+  let swapped = false;
+
+  while (!swapped) {
+    swapped = true;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        const temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = false;
+      }
+    }
+  }
+
+  return arr;
+}
+
+const array = [5, 4, 2, 1];
+
+const result = bubbleSort(array);
+
+console.log(result);
 module.exports = bubbleSort;
--- 09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
+++ 09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
@@ -1,3 +1,24 @@
-function selectionSort() {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
 
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+
+  return arr;
+}
+
+const array = [4, 3, 2, 10, 12, 1, 5, 6];
+
+const result = selectionSort(array);
+
+console.log(result);
 module.exports = selectionSort;
